8c3c551e8872800a3bc5e97400b3ff4b15c141ab
Dora <santiagodora004@gmail.com>
2026-03-26 18:53:53 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
-function displayPoem(response) {
-  console.log("poem generatated");
-  new Typewriter("#poem", {
-    strings: "No Man Is An Island",
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typerwriter("#poem", {
+    strings: [
+      "In the realm of dreams, where shadows dance,",
+      "A poet weaves words in a mystical trance.",
+    ],
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
-
-function generatePoem(event) {
-  event.preventDefault();
-
-  let instructionInput = document.querySelector("#user-instruction");
-  let apiKey = "36399t92b73b5o4fa0b8a2cb798d68aa";
-  let prompt =
-    "You are a romanic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions.";
-  let context = "User instructions: Generate a English poem about roses.";
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
-
-  axios.get(apiURL).then(displayPoem);
-}
-
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
